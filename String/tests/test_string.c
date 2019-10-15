922320c4420641796693c86f9920da222d652457
Centurion256 <vkuchynskyy@gmail.com>
2019-10-15 21:19:43 +0300

    Added a test case for my_str_shrink_to_fit

--- String/tests/test_string.c
+++ String/tests/test_string.c
@@ -39,18 +39,38 @@ START_TEST(string_test_resize_negative)
 }
 END_TEST
 
+START_TEST(string_test_shrink)
+{
+    size_t length = (size_t)10;
+    const char* original_str = my_str_get_cstr(&string);
+    my_str_resize(&string, length, 0);
+    size_t actual_size = my_str_size(&string);
+    ck_assert_int_eq(my_str_shrink_to_fit(&string), 0);
+    ck_assert_int_eq(my_str_size(&string), actual_size);
+    ck_assert_int_eq(my_str_capacity(&string), actual_size);
+    ck_assert_str_eq(my_str_get_cstr(&string), original_str);
+}
+END_TEST
+
 Suite* string_actions_suite(void)
 {
     Suite* suit;
     TCase* tc_resize;
+    TCase* tc_shrink;
 
     suit = suite_create("Memory opertaions, copy, etc.");
+
     tc_resize = tcase_create("Resize tests");
     tcase_add_checked_fixture(tc_resize, setup, teardown);
     tcase_add_test(tc_resize, string_test_resize);
     tcase_add_test(tc_resize, string_test_resize_negative);
 
+    tc_shrink = tcase_create("Shrink tests");
+    tcase_add_checked_fixture(tc_shrink, setup, teardown);
+    tcase_add_test(tc_shrink, string_test_shrink);
+
     suite_add_tcase(suit, tc_resize);
+    suite_add_tcase(suit, tc_shrink);
     return suit;
 }
 
